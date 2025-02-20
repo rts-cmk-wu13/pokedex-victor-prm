25ebafa49e16685f046d4ec2af9cf43be7849af6
victor-prm <victorpabb@gmail.com>
2025-02-20 10:15:19 +0100

    changed basic fetch function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 function fetchPokemon() {
-    fetch("https://pokeapi.co/api/v2/generation/1/")
+    fetch('https://pokeapi.co/api/v2/pokemon')
         .then(response => response.json())
         .then(function (allpokemon) {
-            allpokemon.pokemon_species.forEach(function (pokemon) {
+            allpokemon.results.forEach(function (pokemon) {
                 fetchPokemonData(pokemon);
             })
         })
@@ -10,12 +10,11 @@ function fetchPokemon() {
 
 
 function fetchPokemonData(pokemon) {
-    let url = pokemon.url
+    let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
     fetch(url)
         .then(response => response.json())
         .then(function (pokeData) {
             console.log(pokeData)
-            console.log(pokeData.stats)
         })
 }
 
